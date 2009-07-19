528cf5cd42418966e327d95a6475705b8ae98911
erichamby <erichamby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-19 18:38:23 +0000

    git-svn-id: https://plugins.svn.wordpress.org/forum-server/trunk@136715 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wpf-main.php
@@ -0,0 +1,28 @@
+<?php
+/*
+	Plugin Name: Forum Server
+	Plugin Author: Eric Hamby
+	Plugin URI: http://vasthtml.com
+	Author URI: http://erichamby.com
+	Version: 1.1
+*/
+
+//$plugin_dir = basename(dirname(__FILE__)); 
+//load_plugin_textdomain( 'vasthtml', ABSPATH.'wp-content/plugins/'. $plugin_dir.'/', $plugin_dir.'/' ); 
+include_once("wpf.class.php");
+
+// Short and sweet :)
+$vasthtml = new vasthtml();
+
+// Activating?
+register_activation_hook(__FILE__ ,array(&$vasthtml,'fs_vasthtml_install'));
+
+add_action("the_content", array(&$vasthtml, "go"));
+add_action('init', array(&$vasthtml,'set_cookie'));
+add_filter("wp_title", array(&$vasthtml, "set_pagetitle"));
+function latest_activity($num = 5){
+	global $vasthtml;
+	return $vasthtml->latest_activity($num);
+}
+
+?>
